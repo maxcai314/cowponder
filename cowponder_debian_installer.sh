e478c30813d2439d882a8d9cf03c0d273c3dbf1e
Max Cai <maxster@xz.ax>
2023-09-18 21:50:34 -0700

    rename installer script to cowponder_debian_installer.sh cus it should ONLY be used for debian systems

--- cowponder_debian_installer.sh
+++ cowponder_debian_installer.sh
@@ -3,5 +3,3 @@ TEMP_DEB="$(mktemp)"
 wget -O "$TEMP_DEB" 'https://xz.ax/cowponder_0.0.1-1_all.deb'
 dpkg -i "$TEMP_DEB"
 rm -f "$TEMP_DEB"
-echo
-echo "cowponder may not be runnable as root, because the location of the cowthink runnable may not be in the sudo secure path."
