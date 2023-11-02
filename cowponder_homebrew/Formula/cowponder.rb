459bbb2cf7d08bedd3c1fbecbbe3108bfdee8323
Max Cai <maxster@xz.ax>
2023-11-02 12:42:04 -0700

    updated cowponder --update to use python requests package and error handling

--- cowponder_homebrew/Formula/cowponder.rb
+++ cowponder_homebrew/Formula/cowponder.rb
@@ -2,7 +2,7 @@ class Cowponder < Formula
   desc "Simple terminal command to display random philosophical thoughts from a cow"
   homepage "https://github.com/maxcai314/homebrew-cowponder"
   url "https://max.xz.ax/cowponder/cowponder-homebrew-v0.0.2.tar.gz"
-  sha256 "4ddfbae2d0bfd305753304f1a7e834e84e5d8f7b2a461cba9f216969dcf83810"
+  sha256 "a15a5a8648b0ee4d0a70b6392affa335ae7aa6a8b9e1d259f8582b1b049eeb28"
 
   depends_on "cowsay"
   depends_on "python@3"
