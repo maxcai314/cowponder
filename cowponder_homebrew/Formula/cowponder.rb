4e000bc2c22d034e60b1b49281d96a057cda16da
Max Cai <maxster@xz.ax>
2023-11-02 12:51:54 -0700

    make cowponder -u use ponder -u for consistency and dry

--- cowponder_homebrew/Formula/cowponder.rb
+++ cowponder_homebrew/Formula/cowponder.rb
@@ -2,7 +2,7 @@ class Cowponder < Formula
   desc "Simple terminal command to display random philosophical thoughts from a cow"
   homepage "https://github.com/maxcai314/homebrew-cowponder"
   url "https://max.xz.ax/cowponder/cowponder-homebrew-v0.0.2.tar.gz"
-  sha256 "a15a5a8648b0ee4d0a70b6392affa335ae7aa6a8b9e1d259f8582b1b049eeb28"
+  sha256 "d3fbf78356b027128f61fe9fcd2335a2f76dcf78350f1b326892e7b6e1cfd842"
 
   depends_on "cowsay"
   depends_on "python@3"
