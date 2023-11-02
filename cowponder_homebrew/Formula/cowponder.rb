204718f28d53e987d45c20606971736185891a1d
Max Cai <maxster@xz.ax>
2023-11-02 10:37:45 -0700

    use system to run ponder in cowponder

--- cowponder_homebrew/Formula/cowponder.rb
+++ cowponder_homebrew/Formula/cowponder.rb
@@ -2,7 +2,7 @@ class Cowponder < Formula
   desc "Simple terminal command to display random philosophical thoughts from a cow"
   homepage "https://github.com/maxcai314/homebrew-cowponder"
   url "https://max.xz.ax/cowponder/cowponder-homebrew-v0.0.2.tar.gz"
-  sha256 "a58a046d41044ed6f0b80d35dc9b20301a56b552ef3613bb30a1e38210b73343"
+  sha256 "4ddfbae2d0bfd305753304f1a7e834e84e5d8f7b2a461cba9f216969dcf83810"
 
   depends_on "cowsay"
   depends_on "python@3"
