d02cd263a2abafc2e77c45fd8b6cec51afc2af03
Max Cai <maxster@xz.ax>
2023-11-02 22:01:08 -0700

    rebuild package and install... it is so nice, cus the main Makefile we can just do clean all install and it will fix the checksum for us!
    Co-authored-by: Max Cai <maxster@xz.ax>
    Co-authored-by: max <mc34608@pausd.us>

--- cowponder_homebrew/Formula/cowponder.rb
+++ cowponder_homebrew/Formula/cowponder.rb
@@ -2,7 +2,7 @@ class Cowponder < Formula
   desc "Simple terminal command to display random philosophical thoughts from a cow"
   homepage "https://github.com/maxcai314/homebrew-cowponder"
   url "https://max.xz.ax/cowponder/cowponder-homebrew-v0.0.2.tar.gz"
-  sha256 "3e291542691de0cace9ab3003aa96917a5532ffdee0ca5f81da370a3dc5da938"
+  sha256 "424e713d7a5711b712452c170d9d49c0445626cb82373faa70ade50a0b417255"
   depends_on "cowsay"
   depends_on "python@3"
 
