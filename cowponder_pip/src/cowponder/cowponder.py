ea35283d460ef340087c831998b289c146747d4c
Reid Dye <reid23>
2023-11-03 00:38:21 -0700

    initial progress. doesn't work but this is not a priority atm

---
+++ cowponder_pip/src/cowponder/cowponder.py
@@ -0,0 +1,20 @@
+def cow(eyes="oo", tongue=" "):
+    return (
+f"""      o  ^__^             
+       o ({eyes})\________    
+         (__)\        )\/\\
+          {tongue}   ||----w |   
+              ||     ||   """)
+
+def cowponder(mode=""):
+    faces = dict(
+       b=("==", " "),
+       d=("XX", "U"),
+       g=("$$", " "),
+       p=("@@", " "),
+       s=("**", "U"),
+       y=("..", " ")
+    )
+    if mode:
+        args = faces[mode[-1]]
+        
