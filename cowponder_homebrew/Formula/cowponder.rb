244c4349381a6dfdac7c5f5bb310420d2f0fa303
Max Cai <maxster@xz.ax>
2023-11-27 12:14:29 -0800

    add 'requests' python resource and use virtualenv_install_with_resources, idk if it works, i'll have to test it

--- cowponder_homebrew/Formula/cowponder.rb
+++ cowponder_homebrew/Formula/cowponder.rb
@@ -1,8 +1,16 @@
 class Cowponder < Formula
+  include Language::Python::Virtualenv
+
   desc "Simple terminal command to display random philosophical thoughts from a cow"
   homepage "https://github.com/maxcai314/homebrew-cowponder"
   url "https://max.xz.ax/cowponder/cowponder-homebrew-v0.0.3.tar.gz"
   sha256 "f9ba2818473e0b74d311a809a1c884ecedfeddfe3b62bf7dc898539a3d4e4ab5"
+
+  # requests resource
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
+    sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
+  end
 
   version "0.0.3"
 
@@ -10,6 +18,7 @@ class Cowponder < Formula
   depends_on "python@3"
 
   def install
+    virtualenv_install_with_resources
     bin.install "ponder"
     bin.install "cowponder"
     etc.install "cowthoughts.txt"
