1dfc8d028eb124f87e913c8109f458c0dde7cab2
Max Cai <maxster@xz.ax>
2023-11-27 19:01:12 -0800

    rebuild

--- cowponder_homebrew/Formula/cowponder.rb
+++ cowponder_homebrew/Formula/cowponder.rb
@@ -3,7 +3,7 @@ class Cowponder < Formula
 
   desc "Simple terminal command to display random philosophical thoughts from a cow"
   homepage "https://github.com/maxcai314/homebrew-cowponder"
-  url "https://max.xz.ax/cowponder/cowponder-homebrew-v0.0.3.tar.gz"
+  url "https://max.xz.ax/cowponder/cowponder-homebrew-v0.0.4.tar.gz"
   sha256 "f9ba2818473e0b74d311a809a1c884ecedfeddfe3b62bf7dc898539a3d4e4ab5"
 
   # requests resource
@@ -12,7 +12,7 @@ class Cowponder < Formula
     sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
   end
 
-  version "0.0.3"
+  version "0.0.4"
 
   depends_on "cowsay"
   depends_on "python@3"
