c6b7dbe1a4c7803d3ed2b1c05abbc0f0d50cb46e
Max Cai <maxster@xz.ax>
2023-11-27 19:20:56 -0800

    fkabfle

--- cowponder_homebrew/Formula/cowponder.rb
+++ cowponder_homebrew/Formula/cowponder.rb
@@ -1,10 +1,11 @@
 class Cowponder < Formula
   include Language::Python::Virtualenv
 
-  version "0.0.4"
-
   desc "Simple terminal command to display random philosophical thoughts from a cow"
   homepage "https://github.com/maxcai314/homebrew-cowponder"
+
+  version "0.0.4"
+
   url "https://max.xz.ax/cowponder/cowponder-homebrew-v0.0.4.tar.gz"
   sha256 "f9ba2818473e0b74d311a809a1c884ecedfeddfe3b62bf7dc898539a3d4e4ab5"
 
