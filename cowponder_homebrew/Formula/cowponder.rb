05ffb4b14e133a10f44eb28d4b816059480ba433
Max Cai <maxster@xz.ax>
2023-11-27 19:37:16 -0800

    fawef

--- cowponder_homebrew/Formula/cowponder.rb
+++ cowponder_homebrew/Formula/cowponder.rb
@@ -9,14 +9,14 @@ class Cowponder < Formula
 
   sha256 "f9ba2818473e0b74d311a809a1c884ecedfeddfe3b62bf7dc898539a3d4e4ab5"
 
+  depends_on "cowsay"
+  depends_on "python@3"
+
   # requests resource
   resource "requests" do
     url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
     sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
   end
-
-  depends_on "cowsay"
-  depends_on "python@3"
 
   def install
     virtualenv_install_with_resources
