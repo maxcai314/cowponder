e9ecd38f336a80b13d6da0f9659a446a75012dc4
Max Cai <maxster@xz.ax>
2023-11-27 19:03:13 -0800

    apparently homebrew is stupid and needs version number first

--- cowponder_homebrew/Formula/cowponder.rb
+++ cowponder_homebrew/Formula/cowponder.rb
@@ -1,6 +1,8 @@
 class Cowponder < Formula
   include Language::Python::Virtualenv
 
+  version "0.0.4"
+
   desc "Simple terminal command to display random philosophical thoughts from a cow"
   homepage "https://github.com/maxcai314/homebrew-cowponder"
   url "https://max.xz.ax/cowponder/cowponder-homebrew-v0.0.4.tar.gz"
@@ -11,8 +13,6 @@ class Cowponder < Formula
     url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
     sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
   end
-
-  version "0.0.4"
 
   depends_on "cowsay"
   depends_on "python@3"
