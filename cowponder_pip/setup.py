d230f07169d5bf6e25ca3c9252854c6fb6634392
Max Cai <maxcai.paloalto@gmail.com>
2024-01-19 20:28:53 -0800

    setup.py work

--- cowponder_pip/setup.py
+++ cowponder_pip/setup.py
@@ -14,7 +14,7 @@ setup(
     description="A simple command that displays randomly selected philosophical thoughts from a cow",
     url="https://max.xz.ax/cowponder",
     version="0.0.2",
-    scripts=["cowponder.py", "ponder.py"],
+    scripts=["src/cowponder/cowponder.py", "src/cowponder/ponder.py"],
     install_requires=requirements,
     keywords="cow philosophical ponder cowponder ascii ascii-art fun",
 )
