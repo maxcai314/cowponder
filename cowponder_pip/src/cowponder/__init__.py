fc9f4734905125d7bfdb62a54f7b2a6d53af0313
Max Cai <maxcai.paloalto@gmail.com>
2024-01-19 20:14:52 -0800

    idk

--- cowponder_pip/src/cowponder/__init__.py
+++ cowponder_pip/src/cowponder/__init__.py
@@ -1,5 +1,5 @@
 """
 cowponder
 
-A simple terminal command that displays randomly selected philosophical thoughts from a cow
+A simple command that displays randomly selected philosophical thoughts from a cow
 """
--- cowponder_pip/src/cowponder/setup.py
+++ cowponder_pip/src/cowponder/setup.py
@@ -1,5 +1,20 @@
 from setuptools import setup
 
-# setup(
-#     # stuff here
-# )
+def read_file(filename):
+    try:
+        with open(filename) as readme_file:
+            return readme_file.read()
+    except:
+        return ''
+
+requirements = read_file('requirements.txt')
+
+setup(
+    name="cowponder",
+    description="A simple command that displays randomly selected philosophical thoughts from a cow",
+    url="https://max.xz.ax/cowponder",
+    version="0.0.2",
+    scripts=["cowponder.py", "ponder.py"],
+    install_requires=requirements,
+    keywords="cow philosophical ponder cowponder ascii ascii-art fun",
+)
