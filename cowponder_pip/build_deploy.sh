371882324faf7a540963aac55dea692304cb8d93
Max Cai <maxcai.paloalto@gmail.com>
2025-12-08 17:18:16 -0800

    now on pypi and can be pip installed!!!!!

--- cowponder_pip/build_deploy.sh
+++ cowponder_pip/build_deploy.sh
@@ -1,4 +1,5 @@
 rm -rf build dist cowponder.egg-info
 python setup.py sdist bdist_wheel
 twine upload --repository testpypi dist/*
+# twine upload --repository pypi dist/*
 # do not --skip-existing for testpypi, to allow overwriting existing versions
